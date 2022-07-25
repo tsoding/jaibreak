a57ab8a9c9644da3ec9525530b7ea0f45e43fd31
rexim <reximkut@gmail.com>
2022-07-25 15:34:54 +0700

    Introduce js/ folder

---
+++ js/load.js
@@ -0,0 +1,133 @@
+const NULL64 = 0n;
+const EBADF = 9;
+
+let app = document.getElementById("app");
+let ctx = app.getContext("2d");
+// TODO: read the font size from param.conf somehow
+ctx.font = '37px Alegreya-Regular';
+
+let w = null;
+let context = null;
+let output_buffer = "";
+
+function find_name_by_regexp(exports, prefix) {
+    const re = new RegExp('^'+prefix+'_[0-9a-z]+$');
+    for (let name in exports) {
+        if (re.test(name)) {
+            return exports[name];
+        }
+    }
+    return null;
+}
+
+function make_environment(...envs) {
+    return new Proxy(envs, {
+        get(target, prop, receiver) {
+            for (let env of envs) {
+                if (env.hasOwnProperty(prop)) {
+                    return env[prop];
+                }
+            }
+            return (...args) => {console.error("NOT IMPLEMENTED: "+prop, args)}
+        }
+    });
+}
+
+const std = {
+    "write": (fd, buf, count) => {
+        let log = undefined;
+        switch (fd) {
+            case 1: log = console.log;   break;
+            case 2: log = console.error; break;
+            default: {
+                console.error("write: Unsupported file descriptor "+fd);
+                return -EBADF;
+            }
+        }
+        const buffer = w.instance.exports.memory.buffer;
+        const bytes = new Uint8Array(buffer, Number(buf), Number(count));
+        let text = new TextDecoder().decode(bytes);
+        let index = text.indexOf('\n');
+        while (index >= 0) {
+            output_buffer += text.slice(0, index);
+            text = text.slice(index + 1);
+            log(output_buffer);
+            output = "";
+            index = text.indexOf('\n');
+        }
+        if (text.length > 0) output_buffer += text;
+        return count;
+    },
+    "memset": (s, c, n) => {
+        const buffer = w.instance.exports.memory.buffer;
+        const bytes = new Uint8Array(buffer, Number(s), Number(n));
+        bytes.fill(c);
+        return s;
+    },
+    "fabs": Math.abs,
+    "powf": Math.pow,
+    "set_context": (c) => context = c,
+};
+
+// Game runtime
+const game = {
+    "fill_rect_wasm": (x, y, w, h, r, g, b, a) => {
+        ctx.fillStyle = hexcolor(r, g, b, a);
+        ctx.fillRect(x, y, w, h);
+    },
+    "random_get_zero_to_one": Math.random,
+    "random_get_within_range": (a, b) => a + Math.random()*(b - a),
+    "platform_prepare_text": (data, count) => {
+        const buffer = w.instance.exports.memory.buffer;
+        const text = new TextDecoder().decode(new Uint8Array(buffer, Number(data), Number(count)));
+        prepared_text = text;
+        return BigInt(Math.floor(ctx.measureText(text).width));
+    },
+    "platform_draw_prepared_text": (x, y, r, g, b, a) => {
+        ctx.fillStyle = hexcolor(r, g, b, a);
+        ctx.fillText(prepared_text, Number(x), app.height - Number(y));
+    }
+};
+
+let prepared_text = null;
+
+function hexcolor(r, g, b, a) {
+    r = Math.floor(r*255).toString(16).padStart(2, 0);
+    g = Math.floor(g*255).toString(16).padStart(2, 0);
+    b = Math.floor(b*255).toString(16).padStart(2, 0);
+    a = Math.floor(a*255).toString(16).padStart(2, 0);
+    return '#'+r+g+b+a;
+}
+
+WebAssembly.instantiateStreaming(fetch('./main32.wasm'), {
+    "env": make_environment(std, game)
+}).then(w0 => {
+    w = w0;
+    const init_state  = find_name_by_regexp(w.instance.exports, "init_state");
+    const render      = find_name_by_regexp(w.instance.exports, "render");
+    const update      = find_name_by_regexp(w.instance.exports, "update");
+    const key_press   = find_name_by_regexp(w.instance.exports, "key_press");
+    const key_release = find_name_by_regexp(w.instance.exports, "key_release");
+
+    w.instance.exports.main(0, NULL64);
+
+    let prev = null;
+    function first(timestamp) {
+        prev = timestamp;
+        window.requestAnimationFrame(frame);
+    }
+    function frame(timestamp) {
+        const dt = (timestamp - prev)*0.001;
+        prev = timestamp;
+        update(context, dt);
+        // TODO: read the background from params.conf
+        ctx.fillStyle = '#181818';
+        ctx.fillRect(0, 0, 1600, 900);
+        render(context);
+        window.requestAnimationFrame(frame);
+    }
+    window.requestAnimationFrame(first);
+
+    document.addEventListener('keydown', (e) => key_press(context, e.keyCode));
+    document.addEventListener('keyup', (e) => key_release(context, e.keyCode));
+}).catch(console.error);
--- wasmstub.c
+++ wasmstub.c
@@ -3,3 +3,4 @@ void platform_prepare_text(void) {}
 void platform_draw_prepared_text(void) {}
 void random_get_zero_to_one(void) {}
 void random_get_within_range(void) {}
+void set_context(void) {}
